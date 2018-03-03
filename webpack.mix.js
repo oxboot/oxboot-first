2a320163f5794301dd372f45150e9c18f65889c2
Zorca <vs@zorca.org>
2018-03-03 21:19:46 +0300

    Script & style for default theme added

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath("./");
+
+mix.js('htdocs/content/themes/theme/assets/scripts/theme.js', 'htdocs/content/themes/theme/dist/scripts/theme.js')
+  .sass('htdocs/content/themes/theme/assets/styles/theme.scss', 'htdocs/content/themes/theme/dist/styles/theme.css');
