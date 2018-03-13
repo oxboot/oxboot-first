f20b63984e59a5e785162a86d1e32ea435351494
Zorca <vs@zorca.org>
2018-03-13 17:07:20 +0300

    Oxboot application added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,3 +79,15 @@ if (defined('OXBOOT_ERROR') && OXBOOT_ERROR) {
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
     $whoops->register();
 }
+
+$app = new \Oxboot\Framework\Application($rootDir);
+
+if (! function_exists('oxboot')) {
+    function oxboot($abstract = null, array $parameters = [])
+    {
+        if (is_null($abstract)) {
+            return \Oxboot\Framework\Application::getInstance();
+        }
+        return \Oxboot\Framework\Application::getInstance()->make($abstract, $parameters);
+    }
+}
